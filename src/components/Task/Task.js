3280022ee646870a5054bd38a7443faf33d3a2c3
ThuyLTTb <ThuyLTTb.bkav@gmail.com>
2022-02-23 22:14:33 +0700

    Clean React Architecture

---
+++ src/components/Task/Task.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Task.scss' 
+
+function Task(){
+    return(
+        <li className="task-item">
+              <img src="https://scontent.fhan9-1.fna.fbcdn.net/v/t39.30808-6/248430175_3096981737288492_9035894183556640182_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=7Zgzu9-FUdIAX_tz0Wg&_nc_ht=scontent.fhan9-1.fna&oh=00_AT8SvUold-KYvcl_FpzMgr8mkP4IUk9LBb4urnD-4m9h1Q&oe=621A1AC3"
+             alt="Thuyhanhaio" />
+            Titlre: Lê Thuỳ
+            </li>
+
+    );
+}
+
+
+export default Task;
